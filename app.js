d205bfc2d528959ccc5eced0be5b304f0f30b6a0
VGMEducation <danny.higgins@vgm.com>
2017-05-04 09:41:29 -0500

    App.js Change
    Added comment

--- app.js
+++ app.js
@@ -11,6 +11,7 @@ var cookieParser = require('cookie-parser');
 var bodyParser = require('body-parser');
 //var session = require('express-session')
 
+// added comment for testing of creating a branch
 const http = require('http');
 const https = require('https');
 const fs = require('fs');
